f68aacca944ff7fcd9ea2cd2a81f472c3e4c9336
kklai <kkrebeccalai@gmail.com>
2014-06-16 10:31:30 -0400

    initial commit

---
+++ js/notwork.js
@@ -0,0 +1,5 @@
+var notwork = [{"title":"CSS Dino","link":"http://kklai.github.io/drawings/dinosaur/","imglink":"img/thumbnail/dino.jpg","pub": "Personal", "description":"Drew a dinosaur with Less because good ol' dinos are the best."},
+{"title":"CSS Doggy","link":"http://kklai.github.io/drawings/corgi/","imglink":"img/thumbnail/corgi.jpg","pub": "Personal", "description":"Drew a doggy with Sass during the polar vortex in Chicago."},
+{"title":"Triangle Wall","link":"http://kklai.github.io/triangle-wall/","imglink":"img/thumbnail/triangle.jpg","pub": "Personal", "description":"Something fun with triangles and CSS animation."},
+{"title":"Anger Management","link":"img/anger_management.png","imglink":"img/thumbnail/anger_small.jpg","pub": "Personal", "description":"I was really angry one day and decided to cool down by doing some illustrations."},
+{"title":"The Texas Gifbune","link":"http://texastribuneingifs.tumblr.com/","imglink":"img/thumbnail/julian_beer.gif","pub": "Texas Tribune", "description":"When I worked at the Texas Tribune, I also part-timed as office gif-maker!"}]
