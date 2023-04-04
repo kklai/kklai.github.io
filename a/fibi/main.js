e93af7bc94d1f2f6cc907fc8215b9238b98b6391
Rebecca Lai <rebecca.lai@nytimes.com>
2023-04-04 15:34:54 -0700

    update

--- a/fibi/main.js
+++ a/fibi/main.js
@@ -1,5 +1,7 @@
 console.clear();
 
+var numberOfPoints = 20;
+
 function drawAudio(url, filename) {
   if (!url) {
     url = "audio/230116_0080_U-Bahn.mp3"
@@ -17,7 +19,7 @@ function drawAudio(url, filename) {
   });
   audioTrack.load(url);
   audioTrack.on('ready', function () {
-    var peaks = audioTrack.backend.getPeaks(50);
+    var peaks = audioTrack.backend.getPeaks(numberOfPoints);
     var dist = [];
 
     peaks.forEach(function(peak,i){
@@ -82,7 +84,7 @@ function drawGraph(dist) {
   var audioCont = d3.select(".audio");
   var width = audioCont.node().getBoundingClientRect().width;
   var height = audioCont.node().getBoundingClientRect().height;
-  var x = d3.scaleBand().domain(d3.range(0,50)).range([0,width]).padding(0.1);
+  var x = d3.scaleBand().domain(d3.range(0,numberOfPoints)).range([0,width]).padding(0.1);
   var y = d3.scaleLinear().domain([min,max]).range([height,0]);
   var r = x.bandwidth()/2;
   var svg = sel.append("svg")
@@ -102,6 +104,8 @@ function drawGraph(dist) {
     dotcount.push(dots);
 
     each.appendMany("circle", d3.range(0,dots))
+      .attr("class", "g-circle g-group-" + i)
+      .attr("data-group", i)
       .translate((cd,ci) => [0,r*ci*2])
       .attr("r", r)
   })
@@ -112,55 +116,66 @@ function drawGraph(dist) {
 }
 
 function drawDots(dist, dotcount) {
-  var max = d3.max(dist);
-  var min = d3.min(dist);
-  var totaldist = d3.sum(dist)
-  var cumulativeDist = [];
-  var cum = 0;
-  dist.forEach(function(d){
-    cum += d;
-    cumulativeDist.push(cum);
-  });
-
   var cont = d3.select(".g-a3-cont");
   var contw = cont.node().getBoundingClientRect().width;
   var conth = contw*29.7/42;
-
   var r = Math.sqrt((contw*conth)*((314.16)/(124740))/Math.PI)*2;
   var svg = d3.select(".g-letter.g-active");
   var path = svg.select("path").node();
   var length = path.getTotalLength();
   var circleg = svg.selectAppend("g.cirlceg").html("");
 
-  var dotdens = [];
-  var cum = 0;
-  dotcount.forEach(function(d){
-    d = d/5;
-    var range = d3.range(1,d+1);
-    range.forEach(function(a){
-      cum += 1/d
-      dotdens.push(cum);
-    })
+  console.log(dotcount)
+
+  // var dotdens = [];
+  // var cum = 0;
+  // dotcount.forEach(function(d){
+  //   d = d/10
+  //   var range = d3.range(1,d+1);
+  //   range.forEach(function(a){
+  //     cum += 1/d
+  //     dotdens.push(cum);
+  //   })
+  // })
+  // var totaldist = d3.sum(dotdens);
+  // dotdens.forEach(function(d,i){
+  //   var pt = path.getPointAtLength((d/cum)*length);
+  //   var a = circleg.append("g")
+  //     .attr("transform", "translate(" + pt.x + "," + pt.y + ")")
+  //   a.append("circle")
+  //     .style("fill", "#0000ff")
+  //     // .style("fill", "none")
+  //     // .style("stroke", "#0000ff")
+  //     // .style("stroke-width", 2)
+  //     .attr("r", r)
+  // })
+
+
+  dotcount.forEach(function(d,i){
+
+    for (var j = 0; j < d; j++) {
+      console.log(j, j/d, (i*(1+(j/d))))
+      var pt = path.getPointAtLength((((i+j/d)/dotcount.length))*length);
+      var a = circleg.append("g")
+        .attr("data-group", i)
+        .attr("class", "g-circle g-group-" + i)
+        .attr("transform", "translate(" + pt.x + "," + pt.y + ")")
+        .style("fill", "#0000ff")
+      a.append("circle")
+        .attr("r", r)  
+    }
+    
   })
-  var totaldist = d3.sum(dotdens);
-  dotdens.forEach(function(d,i){
-    var pt = path.getPointAtLength((d/cum)*length);
-    var a = circleg.append("g")
-      .attr("transform", "translate(" + pt.x + "," + pt.y + ")")
-    a.append("circle")
-      // .style("fill", "#0000ff")
-      .style("fill", "none")
-      .style("stroke", "#0000ff")
-      .style("stroke-width", 2)
-      .attr("r", r)
+
+  d3.selectAll(".g-circle").on("click", function(){
+
+    var el = d3.select(this);
+    var group = el.attr("data-group");
+    
+    d3.selectAll(".g-circle").style("fill", "#0000ff")
+    d3.selectAll(".g-group-" + group).style("fill", "#000")
+
   })
-
-  d3.select(".g-canvas-cont").html("");
-
-
-  html2canvas(document.querySelector(".g-a3-cont")).then(canvas => {
-      document.querySelector(".g-canvas-cont").appendChild(canvas)
-  });
 
 }
 
@@ -268,10 +283,15 @@ d3.selectAll(".g-select").on("click", function(){
 
 })
 
-// document.querySelector('.g-print').addEventListener('click', (e) => {
-//   e.preventDefault();
+document.querySelector('.g-print').addEventListener('click', (e) => {
+  e.preventDefault();
+  d3.select(".g-canvas-cont").html("");
 
-// });
+  html2canvas(document.querySelector(".g-a3-cont")).then(canvas => {
+      document.querySelector(".g-canvas-cont").appendChild(canvas)
+  });
+
+});
 
 
 drawAudio();
