3e19263883215c6db22494320261075a489de6ac
KK Rebecca Lai <rebecca.lai@nytimes.com>
2024-08-10 23:01:22 -0700

    stufF

--- a/wavezine-song-quiz/script.js
+++ a/wavezine-song-quiz/script.js
@@ -2,24 +2,35 @@ d3.loadData("quiz.csv", function(err, res){
 
     let data = res[0];
 
-    let sel = d3.select(".g-quiz-cont").html("");
-
     let use = data.filter((d,i) => i < 10);
-
+    let currentQ = 0;
     let correct = 0;
+
+    let sel = d3.select(".g-quiz-cont-inner").html("").style("width", (use.length+2)*100 + "vw");
+
+    let coverdiv = sel.append("div.g-quiz-item.g-cover").append("div.g-quiz-item-inner");
+    coverdiv.append("img.cover").attr("src", "cover.png");
+
+    let intro = coverdiv.append("div.g-intro");
+    intro.append("p").text("村快浪男老登七福哭大貫也因王怕田「合下竹兒主教時」哭首女姐綠呀子魚道羊京黃身笑抓，央己放頭送息入對色實布祖紅穴、羽爸訴示牛飯丟司雞秋經合貝也坐寺冰，頁坡言里。")
+    intro.append("p").text("了東真杯，申停固說婆乾節乞飛黑可沒高書天用坐買再進，字假校教牛姐話兌夕跳能空蝶院拍乞位交「那常夕日乾」喝未正午次我抄菜穴或。")
+
+    coverdiv.append("button.g-next-button.g-cover-button").html("開始估歌仔").on("click", function(){
+        goToNext();
+    });
 
     use.forEach(function(d,i){
 
-        let cont = sel.append("div.g-quiz-item.g-quiz-item-" + i);
+        let cont = sel.append("div.g-quiz-item.g-quiz-item-" + i).append("div.g-quiz-item-inner");
         cont.append("div.g-num").text(i+1)
-        cont.append("div.g-hint").append("div.g-hint-inner").append("img").attr("src", d["Hint (Photo)"])
+        cont.append("div.g-hint").style("background-image", `url(${d['Hint (Photo)']})`).append("div.g-answer").append("div.g-answer-inner");
 
         let qsplit = d.Question.split("Ｘ").join("<span class='guess'></span>").split("//").join("<br>");
         console.log(qsplit)
         cont.append("div.g-q").html(qsplit);
-        cont.append("div.g-answer");
 
-        cont.append("input#input-" + i).attr("type", "string")
+        let inputcont = cont.append("div.g-input-cont");
+        inputcont.append("input#input-" + i).attr("type", "string")
         cont.append("button#button-" + i).text("估！").on("click", function(){
             
             let el = d3.select(this);
@@ -28,18 +39,20 @@ d3.loadData("quiz.csv", function(err, res){
             
             if (input == '') { 
 
-                inputel.transition().style("background", "red").transition().style("background", "white")
+                inputel.transition().style("background", "#ffcc00").transition().style("background", "white")
                 
             } else {
 
                 if (input == d.Answer) {
-                    el.classed("correct", true);
+                    inputcont.classed("correct", true);
                     el.text("啱咗 ✔");
                     correct += 1;
+                    sel.select(".g-score").html(correct);
                 } else {
-                    el.classed("wrong", true);
-                    el.text("錯咗 ✘");
+                    inputcont.classed("wrong", true);
                 }
+
+                el.text("下一條");
 
                 let split = d.Answer.split("");
                 cont.selectAll(".guess").each(function(d,i){
@@ -47,27 +60,47 @@ d3.loadData("quiz.csv", function(err, res){
                 });
 
                 let title = d["Hint (Title)"] == '' ? d.Answer : d["Hint (Title)"];
-                cont.select(".g-answer").text(`${title} - ${d["Hint (Singer)"]}`);
+                cont.select(".g-answer").classed("g-active", true);
+                cont.select(".g-answer-inner").html(`${title}<br>${d["Hint (Singer)"]}`);
 
-
-
-                if (i == use.length -1) {
-                    sel.append("div.g-summary")
-                        .text(`恭喜你！ ${use.length} 條答中咗 ${correct} 條！`)
-                }
-
-            
+                el.on("click", function() {
+                    goToNext();
+                })
             }
-
         })
 
-
-
-        // cont.append("div.g-answer.g-hidden")
-        //     .text("")
-
-
-
     })
+
+    let enddiv = sel.append("div.g-quiz-item.g-end").append("div.g-quiz-item-inner");
+    enddiv.append("img.cover").attr("src", "cover.png");
+
+    let score = enddiv.append("div.g-score-cont");
+    score.append("p").html(`恭喜你！<br>${use.length} 條答中咗 <span class='g-score'>${correct}</span> 條！`)
+
+    function goToNext() {
+        currentQ += 1;
+
+        if (currentQ < use.length + 2) {
+            sel.style("transform", `translate(-${currentQ*100}vw,0`);
+        } else {
+            currentQ = use.length + 2;
+        }
+    }
+
+    document.onkeydown = checkKey;
+
+    function checkKey(e) {
+
+        e = e || window.event;
+
+        if (e.keyCode == '37') {
+        // left arrow
+        }
+        else if (e.keyCode == '39') {
+        // right arrow
+        goToNext()
+        }
+
+    }
 
 })
